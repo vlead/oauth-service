65814f92a00e5a9d4398c61840fe89dca55cb1df
shubhMaheshwari <maheshwarishubh98@gmail.com>
2017-10-31 12:05:35 +0530

    Added Identity Providers for SSO

--- src/vlead-cas-overlay/run.sh
+++ src/vlead-cas-overlay/run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+cp ../../../src/vlead-cas-overlay/public_private_keys/thekeystore public_private_keys/thekeystore   
+cp ../../../src/vlead-cas-overlay/vlead-cas-server/src/main/resources/truststore.jks vlead-cas-server/src/main/resources/
+
 sudo fuser -k 8443/tcp
 sudo fuser -k 8081/tcp
 
 mvn clean package
-sudo java -jar cas-overlay-server-demo/target/cas.war &
-sudo java -jar cas-overlay-management-demo/target/cas-management.war 
+
+java -jar -Djava.net.useSystemProxies=true vlead-cas-server/target/cas.war
+
+java -jar -Djava.net.useSystemProxies=true cas-overlay-management-demo/target/cas-management.war 
