88ad7756ad5af9939ad233e0ae52122739a4663f
shubhMaheshwari <maheshwarishubh98@gmail.com>
2017-11-02 07:38:00 +0530

    Added scripts for ads-centos

---
+++ scripts/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export HTTP_PROXY=http://proxy.iiit.ac.in:8080/
+export http_proxy=http://proxy.iiit.ac.in:8080/
+export FTP_PROXY=http://proxy.iiit.ac.in:8080/
+export all_proxy=socks://proxy.iiit.ac.in:8080/
+export HTTPS_PROXY=http://proxy.iiit.ac.in:8080/
+export https_proxy=http://proxy.iiit.ac.in:8080/
+export  no_proxy=::1,localhost,127.0.0.1/8,0.0.0.0,iiit.ac.in,*.iiit.ac.in,iiit.net,*.iiit.net,iiith.org,172.16.0.0/12,192.168.0.0/16,10.0.0.0/8
+export ALL_PROXY=socks://proxy.iiit.ac.in:8080/
+export  ftp_proxy=http://proxy.iiit.ac.in:8080/
